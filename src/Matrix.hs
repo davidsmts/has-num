f855404da1629ca22007dbff889abbfe65e1e31e
David Schmotz <davidschmotz@gmx.de>
2019-10-30 19:59:06 +0100

    - zeroMatrix, xMatrix, changeXtoY, change for index doesnt work, vector

---
+++ src/Matrix.hs
@@ -0,0 +1,95 @@
+module Matrix (
+    transpose,
+    matrixMult
+) where
+
+
+-- upperTri :: Int -> [[Int]]
+-- upperTri dim
+--     | 
+--     | otherwise = 
+
+xMatrix :: Int -> Int -> Int -> [[Int]]
+xMatrix rowDim colDim val
+    | rowDim == 0   = []
+    | otherwise     = (xVector colDim val) : (xMatrix (rowDim-1) colDim val)
+
+zero :: Int -> Int -> [[Int]]
+zero rowDim colDim = xMatrix rowDim colDim 0
+
+
+--
+-- Vector stuff
+--
+
+changeXtoY :: [Int] -> Int -> Int -> [Int]
+changeXtoY (x:xs) ix y
+    | xs == []  = if (x == ix)
+                    then y : [] else x: []
+    | ix == x   = y : changeXtoY xs ix y
+    | otherwise = x : changeXtoY xs ix y
+
+-- doesnt work yet
+changeItoY :: [Int] -> Int -> Int -> [Int]
+changeItoY (x:xs) i y
+    | xs == []          = if ((length xs) == i )
+                            then y : [] else x: []
+    | (length xs) == i  = y : changeItoY xs i y
+    | otherwise         = x : changeItoY xs i y
+
+fillOneV :: Int -> Int -> Int -> [Int]
+fillOneV x y dim
+    | dim == 0  = []
+    | y == dim  = x : fillOneV x y (dim-1)
+    | otherwise = 0 : fillOneV x y (dim-1)
+
+xVector :: Int -> Int -> [Int]
+xVector dim val
+    | dim == 0  = []
+    | otherwise = val : xVector (dim - 1) val
+
+zeroV :: Int -> [Int]
+zeroV dim = xVector dim 0
+
+-- onesTill :: Int -> Int -> [Int]
+-- onesTill dim a
+--     | a == 0    = 
+--     | otherwise = 
+
+-- onesFrom :: Int -> [Int]
+
+transpose :: [[Int]] -> [[Int]]
+transpose (x:xs)
+    | xs == []  = arrPlode x
+    | otherwise = arrConn (arrPlode x) (transpose xs)
+
+
+arrPlode :: [Int] -> [[Int]]
+arrPlode (x:xs)
+    | xs == []  = [[x]]
+    | otherwise = [x] : (arrPlode xs)
+
+arrConn :: [[Int]] -> [[Int]] -> [[Int]]
+arrConn (x:xs) (y:ys)
+    | xs == []  = [x ++ y]
+    | otherwise = (x ++ y) : (arrConn xs ys)
+
+
+matrixMult :: [[Int]] -> [[Int]] -> [[Int]]
+matrixMult a b = matrixMultTransposed a (transpose b)
+
+
+matrixMultTransposed  :: [[Int]] -> [[Int]] -> [[Int]]
+matrixMultTransposed (x:xs) b
+    | xs == [] = rowByColumns x b : []
+    | otherwise = (rowByColumns x b) : (matrixMultTransposed xs b)
+
+
+rowByColumns :: [Int] -> [[Int]] -> [Int]
+rowByColumns a (x:xs)
+    | xs == [] = (rowByColumn a x) : []
+    | otherwise = (rowByColumn a x) : (rowByColumns a xs)
+
+rowByColumn :: [Int] -> [Int] -> Int
+rowByColumn [] [] = 0
+rowByColumn (x:xs) (y:ys) = x*y + (rowByColumn xs ys)
