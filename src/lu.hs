d103544db2739b889810fdbba6ba4477cad5e425
David Schmotz <davidschmotz@gmx.de>
2019-11-03 20:29:10 +0100

    - l-matrix

--- src/lu.hs
+++ src/lu.hs
@@ -10,17 +10,20 @@ import Matrix
 -- lu (x:xs)
 --     | xs == [] = ()
 
--- l :: [[Int]] -> Int -> Int -> [[Int]]
--- l (x:xs) i
---     | xs == []  = []
---     | otherwise = 
+l :: [[Int]] -> Int -> [[Int]]
+l (x:xs) i 
+    | xs == []  = (oand1till x 0 i) : []
+    | otherwise = (oand1till x 0 i) : (l xs (i+1))
 
 oand1till :: [Int] -> Int -> Int -> [Int]
 oand1till (x:xs) i till
     | xs == []  = if i <= till then (1 : []) else (x : [])
     | i < till = 0 : (oand1till xs (i+1) till)
     | i == till = 1 : (oand1till xs (i+1) till)
-    | otherwise = x : (oand1till xs i till) 
+    | otherwise = x : (oand1till xs i till)
+
+
+
 -- lu ::  [[Int]] -> ([[Int]], [[Int]])
 -- lu (x:xs) = 
 --     | xs == [] = (1,x)
