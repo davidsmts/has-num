98db8b6f607173ffbe61202775df73a8723c58d3
David Schmotz <davidschmotz@gmx.de>
2019-11-03 20:34:24 +0100

    - l is actually r

--- src/lu.hs
+++ src/lu.hs
@@ -10,18 +10,17 @@ import Matrix
 -- lu (x:xs)
 --     | xs == [] = ()
 
-l :: [[Int]] -> Int -> [[Int]]
-l (x:xs) i 
+r :: Matrix -> Int -> Matrix
+r (x:xs) i 
     | xs == []  = (oand1till x 0 i) : []
     | otherwise = (oand1till x 0 i) : (l xs (i+1))
 
-oand1till :: [Int] -> Int -> Int -> [Int]
+oand1till :: Vector -> Int -> Int -> Vector
 oand1till (x:xs) i till
     | xs == []  = if i <= till then (1 : []) else (x : [])
     | i < till = 0 : (oand1till xs (i+1) till)
-    | i == till = 1 : (oand1till xs (i+1) till)
+    | i == till = x : (oand1till xs (i+1) till)
     | otherwise = x : (oand1till xs i till)
-
 
 
 -- lu ::  [[Int]] -> ([[Int]], [[Int]])
