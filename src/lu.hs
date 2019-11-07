5cb5a3d299abc2cf55f5062a51ea852540868e35
David Schmotz <davidschmotz@gmx.de>
2019-11-07 21:59:32 +0100

    Revert "- lu wojrs except for rounding"
    This reverts commit 136b407924493baf731a446c9544460f0f07a807.

--- src/lu.hs
+++ src/lu.hs
@@ -6,22 +6,14 @@ module LU
 import Vector
 import Matrix
 
-lu :: Matrix -> (Matrix, Matrix)
-lu a = luz a 0
+-- lu :: [[Int]] -> ([[Int]], [[Int]])
+-- lu (x:xs)
+--     | xs == [] = ()
 
-luz :: Matrix -> Int -> (Matrix, Matrix)
-luz a i
-    | i == length a -1 = (lk, rk : [])
-    | otherwise     = (arrConn lk (fst (luz next (i+1))), rk : snd (luz next (i+1)))
-    where 
-        rk = r a i
-        lk = l a i
-        next = matrixSub a (matrixMult lk [rk])
-
-r :: Matrix -> Int -> Vector
+r :: Matrix -> Int -> Matrix
 r (x:xs) i 
-    | xs == []  = (oand1till x 0 i)
-    | otherwise = (oand1till x 0 i)
+    | xs == []  = (oand1till x 0 i) : []
+    | otherwise = (oand1till x 0 i) : (r xs (i+1))
 
 oand1till :: Vector -> Int -> Int -> Vector
 oand1till (x:xs) i till
@@ -33,27 +25,6 @@ oand1till (x:xs) i till
 l :: Matrix -> Int -> Matrix
 l a i
     | (length a)-1 == i = vecPlode (oand1till (head a) 0 i)
-    | otherwise         = (vecPlode (oand1till (vectorDiv (column a i) ak) 0 i))
+    | otherwise         = arrConn (vecPlode (oand1till (vectorDiv (column a i) ak) 0 i)) (l a (i+1))
     where ak = (a !! i) !! i -- diagonal element
-
-
-
-
--- r :: Matrix -> Int -> Matrix
--- r (x:xs) i 
---     | xs == []  = (oand1till x 0 i) : []
---     | otherwise = (oand1till x 0 i) : (r xs (i+1))
-
--- oand1till :: Vector -> Int -> Int -> Vector
--- oand1till (x:xs) i till
---     | xs == []  = if i <= till then (1 : []) else (x : [])
---     | i < till = 0 : (oand1till xs (i+1) till)
---     | i == till = x : (oand1till xs (i+1) till)
---     | otherwise = x : (oand1till xs i till)
-
--- l :: Matrix -> Int -> Matrix
--- l a i
---     | (length a)-1 == i = vecPlode (oand1till (head a) 0 i)
---     | otherwise         = arrConn (vecPlode (oand1till (vectorDiv (column a i) ak) 0 i)) (l a (i+1))
---     where ak = (a !! i) !! i -- diagonal element
 
