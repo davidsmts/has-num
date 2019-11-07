b0a8489f9a1ef4280e12fd9f0b27ecb824ab1736
David Schmotz <davidschmotz@gmx.de>
2019-11-07 21:57:26 +0100

    - type change ideas.. unsuccessfull

--- src/testFunc.hs
+++ src/testFunc.hs
@@ -4,7 +4,11 @@
 -- vector creation
 --
 
-xVector :: Int -> a -> [a]
+{-# LANGUAGE GADTs #-}
+data Vector a where
+    Vector :: Num a => [a] -> Vector a
+
+xVector :: Int -> a -> Vector a
 xVector dim val
     | dim == 0  = []
     | otherwise = val : xVector (dim - 1) val
