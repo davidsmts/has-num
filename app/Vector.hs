cadb0c9530986b2b51e27cfdf9026af2265cb92e
David Schmotz <davidschmotz@gmx.de>
2019-11-17 22:39:27 +0100

    - fixing vector functions

--- app/Vector.hs
+++ app/Vector.hs
@@ -1,30 +1,45 @@
-module Vector (
-    changeXtoY,
-    changeItoY,
-    xVector,
-    zeroV,
-    fillOneV,
-    Vector,
-    vectorDiv
-) where
+-- module Vector (
+--     changeXtoY,
+--     changeItoY,
+--     xVector,
+--     zeroV,
+--     fillOneV,
+--     Vector,
+--     vectorDiv
+-- ) where
 
     
 --
 -- create vector type
 --
-type Vector = [Int]
+data Vector a = Vector {
+    values :: [a]
+    } deriving (Eq, Show, Ord)
+
+instance (Ord a, Num a) => Num (Vector a)
+    where 
+        (Vector a) + (Vector b) = Vector $ zipWith (+) a b
+        (Vector a) - (Vector b) = Vector $ zipWith (-) a b
+        (Vector a) * (Vector b) = Vector $ zipWith (*) a b
+        -- negate (Vector a)       = Vector $ zipWith (-) (zero (length a)) a
+        abs (Vector a)          = Vector $ vecAbs a
+        signum                  = undefined
+        fromInteger             = undefined
+
 --
 
 --
 -- vector creation
 --
 
-xVector :: Int -> a -> Vector a
+-- Create Vector of dimension: (dim) that only holds values: (val).
+xVector :: Num a => Int -> a -> Vector a
 xVector dim val
-    | dim == 0  = []
-    | otherwise = val : xVector (dim - 1) val
+    | dim == 0  = (Vector [])
+    | otherwise = Vector $ (val : values (xVector (dim - 1) val))
 
-zeroV :: Int -> Vector Int
+
+zeroV :: Num a => Int -> Vector a
 zeroV dim = xVector dim 0
 
 -- todo
@@ -41,41 +56,32 @@ zeroV dim = xVector dim 0
 -- Vector operations
 --
 
-vectorAdd :: Vector a -> Vector a -> Vector a
-vectorAdd (x:xs) (y:ys)
-    | xs == []  = x+y : []
-    | otherwise = x+y : vectorAdd xs ys
+vecAbs :: (Ord a,Num a) => [a] ->  [a]
+vecAbs [] = []
+vecAbs (x:xs)
+    | x < 0     = -x : vecAbs xs
+    | otherwise = x : vecAbs xs
 
-
-vectorSub :: Vector a -> Vector a -> Vector a
-vectorSub (x:xs) (y:ys)
-    | xs == []  = x-y : []
-    | otherwise = x-y : vectorSub xs ys
-
-vectorDiv :: Vector a -> Int -> Vector a
-vectorDiv (x:xs) quotient
-    | xs == []  = (x `div` quotient) : []
-    | otherwise = (x `div` quotient) : (vectorDiv xs quotient)
-
-changeXtoY :: Vector a -> Int -> Int -> Vector a
-changeXtoY (x:xs) ix y
+-- Changes all appearances of a certain number, to another number
+changeXtoY :: (Eq a, Num a) => Vector a -> a -> a -> Vector a
+changeXtoY (Vector (x:xs)) ix y
     | xs == []  = if (x == ix)
-                    then y : [] else x: []
-    | ix == x   = y : changeXtoY xs ix y
-    | otherwise = x : changeXtoY xs ix y
+                    then Vector [y] else Vector [x]
+    | ix == x   = Vector (y : values (changeXtoY (Vector xs) ix y))
+    | otherwise = Vector (x : values (changeXtoY (Vector xs) ix y))
 
-changeItoY :: Vector a -> Int -> Int -> Vector a
-changeItoY list i y = changeItoYunder list (length list - i - 1) y
+-- changeItoY :: Vector a -> Int -> Int -> Vector a
+-- changeItoY list i y = changeItoYunder list (length list - i - 1) y
 
-changeItoYunder :: Vector a -> Int -> Int -> Vector a
-changeItoYunder (x:xs) i y
-    | xs == []          = if ((length xs) == i )
-                            then y : [] else x: []
-    | (length xs) == i  = y : changeItoYunder xs i y
-    | otherwise         = x : changeItoYunder xs i y
+-- changeItoYunder :: Vector a -> Int -> Int -> Vector a
+-- changeItoYunder (x:xs) i y
+--     | xs == []          = if ((length xs) == i )
+--                             then y : [] else x: []
+--     | (length xs) == i  = y : changeItoYunder xs i y
+--     | otherwise         = x : changeItoYunder xs i y
 
-fillOneV :: Int -> Int -> Int -> Vector a
-fillOneV x y dim
-    | dim == 0  = []
-    | y == dim  = x : fillOneV x y (dim-1)
-    | otherwise = 0 : fillOneV x y (dim-1)
+-- fillOneV :: Int -> Int -> Int -> Vector a
+-- fillOneV x y dim
+--     | dim == 0  = []
+--     | y == dim  = x : fillOneV x y (dim-1)
+--     | otherwise = 0 : fillOneV x y (dim-1)
