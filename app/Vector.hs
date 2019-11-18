6a18248b46c365024c213c424f150fcda4f4769a
David Schmotz <davidschmotz@gmx.de>
2019-11-18 10:56:37 +0100

    - works

--- app/Vector.hs
+++ app/Vector.hs
@@ -63,7 +63,7 @@ vecAbs (x:xs)
     | otherwise = x : vecAbs xs
 
 -- Changes all appearances of a certain number, to another number
-changeXtoY :: (Eq a, Num a) => Vector a -> a -> a -> Vector a
+changeXtoY :: (Eq a,Num a) => Vector a -> a -> a -> Vector a
 changeXtoY (Vector (x:xs)) ix y
     | xs == []  = if (x == ix)
                     then Vector [y] else Vector [x]
@@ -73,21 +73,22 @@ changeXtoY (Vector (x:xs)) ix y
 -- todo put changeItoY and changeItoYunder into one function
 -- Changes number at:i to value:y
 -- parent function to changeItoYunder
-changeItoY :: Num a => Vector a -> Int -> a -> Vector a
+changeItoY :: (Eq a, Num a) => Vector a -> Int -> a -> Vector a
 changeItoY list i y = changeItoYunder list ((length $ values list)-i-1) y
 
 -- Changes number at:i to value:y with index 
 -- child function to changeItoY
-changeItoYunder :: Num a => Vector a -> Int -> a -> Vector a
+changeItoYunder :: (Eq a, Num a) => Vector a -> Int -> a -> Vector a
 changeItoYunder (Vector (x:xs)) i y
     | xs == []          = if ((length xs) == i )
                             then Vector [y] else Vector [x]
     | (length $ xs) == i  = Vector $ y : values (changeItoYunder (Vector xs) i y)
     | otherwise         = Vector $ x : values (changeItoYunder (Vector xs) i y)
 
--- Create vector where entry at index:y has the value of:x
-fillOneV :: Num a => a -> Int -> Int -> Vector a
+-- todo fix reverse indexiation
+-- Create vector where entry at index:y has the value of:x and dimension: dim
+fillOneV :: (Eq a, Num a) => a -> Int -> Int -> Vector a
 fillOneV x y dim
-    | dim == 0  = []
-    | y == dim  = x : fillOneV x y (dim-1)
-    | otherwise = 0 : fillOneV x y (dim-1)
+    | dim == 0  = Vector []
+    | y == dim  = Vector $ x : values (fillOneV x y (dim-1))
+    | otherwise = Vector $ 0 : values (fillOneV x y (dim-1))
