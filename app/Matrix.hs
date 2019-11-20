bcff5039c752f1ff2d8a4463f2a71367831da9df
David Schmotz <davidschmotz@gmx.de>
2019-11-20 18:34:09 +0100

    - matrix type with add and sub

--- app/Matrix.hs
+++ app/Matrix.hs
@@ -1,13 +1,13 @@
 module Matrix (
-    transpose,
-    matrixMult,
-    unit,
-    zero,
-    xMatrix,
-    Matrix,
-    column,
-    arrConn,
-    vecPlode
+    -- transpose,
+    -- matrixMult,
+    -- unit,
+    -- zero,
+    -- xMatrix,
+    -- Matrix,
+    -- column,
+    -- arrConn,
+    -- vecPlode
 ) where
 
 -- imports
@@ -15,33 +15,65 @@ import Vector
 
 -- Matrix type
 -- Row ordered
-type Matrix = [[Int]]
+--
+data Matrix a = Matrix {
+    vals :: [[a]]
+    } deriving (Eq, Show, Ord)
+
+instance (Ord a, Num a) => Num (Matrix a)
+    where
+        a + b = matrixAdd a b
+        a - b = matrixSub a b
+        -- (Matrix a) * (Matrix b) = Matrix $ matrixMult a b
+        -- negate (Vector a)       = Vector $ zipWith (-) (zero (length a)) a
+        -- abs (Vector a)          = Vector $ vecAbs a
+        signum                  = undefined
+        fromInteger             = undefined
+
+
+-- matrixAdd :: (Ord a, Num a) => Matrix a -> Matrix a -> Matrix a
+-- matrixAdd (Matrix (x:xs)) (Matrix(y:ys))
+--     | length xs == 0  = Matrix $ values ((Vector x) + (Vector y)) : []
+--     | otherwise = Matrix (values ((Vector x) + (Vector y)) : vals (matrixAdd (Matrix xs) (Matrix ys)))
+
+matrixAdd :: Num a => Matrix a -> Matrix a -> Matrix a
+matrixAdd (Matrix (x:xs)) (Matrix(y:ys))
+    | length xs == 0  = Matrix $ zipWith (+) x y : []
+    | otherwise = Matrix $ zipWith (+) x y : vals (matrixAdd (Matrix xs) (Matrix ys))
+    
+matrixSub :: Num a => Matrix a -> Matrix a -> Matrix a
+matrixSub (Matrix (x:xs)) (Matrix(y:ys))
+    | length xs == 0  = Matrix $ zipWith (-) x y : []
+    | otherwise = Matrix $ zipWith (-) x y : vals (matrixSub (Matrix xs) (Matrix ys))
+        
+        
+    
 
 
 -- Matrix creation
 
-xMatrix :: Int -> Int -> Int -> Matrix
-xMatrix rowDim colDim val
-    | rowDim == 0   = []
-    | otherwise     = (xVector colDim val) : (xMatrix (rowDim-1) colDim val)
+-- xMatrix :: Int -> Int -> Int -> Matrix
+-- xMatrix rowDim colDim val
+--     | rowDim == 0   = []
+--     | otherwise     = (xVector colDim val) : (xMatrix (rowDim-1) colDim val)
 
-zero :: Int -> Int -> Matrix
-zero rowDim colDim = xMatrix rowDim colDim 0
+-- zero :: Int -> Int -> Matrix
+-- zero rowDim colDim = xMatrix rowDim colDim 0
 
-unit :: Int -> Matrix
-unit dim = unitCnt dim dim
+-- unit :: Int -> Matrix
+-- unit dim = unitCnt dim dim
 
-unitCnt :: Int -> Int -> Matrix
-unitCnt dim dimCnt
-    | dimCnt-1 == 0   = fillOneV 1 dimCnt dim : []
-    | otherwise     = (fillOneV 1 dimCnt dim) : (unitCnt dim (dimCnt-1))
+-- unitCnt :: Int -> Int -> Matrix
+-- unitCnt dim dimCnt
+--     | dimCnt-1 == 0   = fillOneV 1 dimCnt dim : []
+--     | otherwise     = (fillOneV 1 dimCnt dim) : (unitCnt dim (dimCnt-1))
 
 
--- other
-column :: Matrix -> Int -> Vector
-column (x:xs) i
-    | xs == []  = x !! i : []
-    | otherwise = x !! i : (column xs i)
+-- -- other
+-- column :: Matrix -> Int -> Vector
+-- column (x:xs) i
+--     | xs == []  = x !! i : []
+--     | otherwise = x !! i : (column xs i)
 
 --todo
 -- upperTri :: Int -> [[Int]]
@@ -52,44 +84,44 @@ column (x:xs) i
 
 -- mathematical operations
 
-transpose :: Matrix -> Matrix
-transpose (x:xs)
-    | xs == []  = vecPlode x
-    | otherwise = arrConn (vecPlode x) (transpose xs)
+-- transpose :: Matrix -> Matrix
+-- transpose (x:xs)
+--     | xs == []  = vecPlode x
+--     | otherwise = arrConn (vecPlode x) (transpose xs)
 
 
-vecPlode :: Vector -> Matrix
-vecPlode (x:xs)
-    | xs == []  = [[x]]
-    | otherwise = [x] : (vecPlode xs)
+-- vecPlode :: Vector -> Matrix
+-- vecPlode (x:xs)
+--     | xs == []  = [[x]]
+--     | otherwise = [x] : (vecPlode xs)
 
-arrConn :: Matrix -> Matrix -> Matrix
-arrConn (x:xs) (y:ys)
-    | xs == []  = [x ++ y]
-    | otherwise = (x ++ y) : (arrConn xs ys)
+-- arrConn :: Matrix -> Matrix -> Matrix
+-- arrConn (x:xs) (y:ys)
+--     | xs == []  = [x ++ y]
+--     | otherwise = (x ++ y) : (arrConn xs ys)
 
 -- row-ordered
-matrixMult :: Matrix -> Matrix -> Matrix
-matrixMult a b = matrixMultTransposed a (transpose b)
+-- matrixMult :: Matrix -> Matrix -> Matrix
+-- matrixMult a b = matrixMultTransposed a (transpose b)
 
 
-matrixAdd :: Matrix -> Matrix -> Matrix
-matrixAdd (x:xs) (y:ys)
-    | xs == []  = vectorAdd x y : []
-    | otherwise = vectorAdd x y : matrixAdd xs ys
-    
+-- matrixSub :: Matrix -> Matrix -> Matrix
+-- matrixSub (x:xs) (y:ys)
+--     | xs == []  = vectorAdd x y : []
+--     | otherwise = vectorAdd x y : matrixAdd xs ys
+        
 
-matrixMultTransposed  :: Matrix -> Matrix -> Matrix
-matrixMultTransposed (x:xs) b
-    | xs == [] = rowByColumns x b : []
-    | otherwise = (rowByColumns x b) : (matrixMultTransposed xs b)
+-- matrixMultTransposed  :: Matrix -> Matrix -> Matrix
+-- matrixMultTransposed (x:xs) b
+--     | xs == [] = rowByColumns x b : []
+--     | otherwise = (rowByColumns x b) : (matrixMultTransposed xs b)
 
 
-rowByColumns :: Vector -> Matrix -> Vector
-rowByColumns a (x:xs)
-    | xs == [] = (rowByColumn a x) : []
-    | otherwise = (rowByColumn a x) : (rowByColumns a xs)
+-- rowByColumns :: Vector -> Matrix -> Vector
+-- rowByColumns a (x:xs)
+--     | xs == [] = (rowByColumn a x) : []
+--     | otherwise = (rowByColumn a x) : (rowByColumns a xs)
 
-rowByColumn :: Vector -> Vector -> Int
-rowByColumn [] [] = 0
-rowByColumn (x:xs) (y:ys) = x*y + (rowByColumn xs ys)
+-- rowByColumn :: Vector -> Vector -> Int
+-- rowByColumn [] [] = 0
+-- rowByColumn (x:xs) (y:ys) = x*y + (rowByColumn xs ys)
--- app/Vector.hs
+++ app/Vector.hs
@@ -4,7 +4,7 @@ module Vector (
     xVector,
     zeroV,
     fillOneV,
-    Vector,
+    Vector (..),
     vectorDiv
 ) where
 
