90d2005e16d31008c6b4821b336f70dd6754b2a5
David Schmotz <davidschmotz@gmx.de>
2019-12-06 19:37:11 +0100

    - cholesky erweitert

--- app/Factorization/Cholesky.hs
+++ app/Factorization/Cholesky.hs
@@ -1,14 +1,27 @@
 module Cholesky (
-
+    cholesky
 ) where
 
 import Matrix
 
--- cholesky :: Num a => Matrix a -> Matrix a
--- cholesky a
---     |
---     | otherwise = (column a 0) : tail (values a)
+cholesky :: Num a => Matrix a -> Matrix a
+cholesky a = cholesky_iterator a (length values a)
 
+cholesky_iterator :: Num a => Matrix a -> Int -> Matrix a
+cholesky_iterator (Matrix x) i
+    | length x - 2 == i = cholesky_col (Matrix x) i : []
+    | otherwise         = cholesky_col (Matrix x) i : cholesky_iterator (Matrix x) (i-1)
+
+cholesky_col :: Floating a => Matrix a -> Int -> Matrix a
+cholesky_col (Matrix x) i
+    | i == 0    = column_ops 
+    | i == lenx = 
+    | otherwise = 
+    where
+        lenx = length x
+        diag = (x !! i) !! i
+        
+-- todo umschreiben, sodass von vorne nach hinten abgearbeitet wird
 column_ops :: Floating a => [a] -> [a] -> [a]
 column_ops arr [] = []
 column_ops arr (x:xs)
